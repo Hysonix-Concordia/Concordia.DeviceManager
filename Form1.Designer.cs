437570abf9e9e41b9c6d33f097f08a066f8c2141
dhysong <dhysong@gmail.com>
2017-07-15 21:16:38 -0400

    Just about have the device manager v1 working

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,142 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtSSID = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtSubscriptionId = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.pnlConfigure = new System.Windows.Forms.Panel();
+            this.pnlConfigure.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtLog
+            // 
+            this.txtLog.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtLog.Location = new System.Drawing.Point(1, 184);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(404, 174);
+            this.txtLog.TabIndex = 0;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.ForeColor = System.Drawing.Color.Red;
+            this.lblStatus.Location = new System.Drawing.Point(299, 14);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(99, 13);
+            this.lblStatus.TabIndex = 1;
+            this.lblStatus.Text = "Listening for device";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(57, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "SSID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(36, 44);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Password";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 70);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Subscription Id";
+            // 
+            // txtSSID
+            // 
+            this.txtSSID.Location = new System.Drawing.Point(95, 15);
+            this.txtSSID.Name = "txtSSID";
+            this.txtSSID.Size = new System.Drawing.Size(251, 20);
+            this.txtSSID.TabIndex = 5;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(95, 41);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(251, 20);
+            this.txtPassword.TabIndex = 6;
+            // 
+            // txtSubscriptionId
+            // 
+            this.txtSubscriptionId.Location = new System.Drawing.Point(95, 67);
+            this.txtSubscriptionId.Name = "txtSubscriptionId";
+            this.txtSubscriptionId.Size = new System.Drawing.Size(251, 20);
+            this.txtSubscriptionId.TabIndex = 7;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(143, 104);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 8;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // pnlConfigure
+            // 
+            this.pnlConfigure.Controls.Add(this.txtSSID);
+            this.pnlConfigure.Controls.Add(this.btnSave);
+            this.pnlConfigure.Controls.Add(this.label2);
+            this.pnlConfigure.Controls.Add(this.txtSubscriptionId);
+            this.pnlConfigure.Controls.Add(this.label3);
+            this.pnlConfigure.Controls.Add(this.txtPassword);
+            this.pnlConfigure.Controls.Add(this.label4);
+            this.pnlConfigure.Location = new System.Drawing.Point(11, 31);
+            this.pnlConfigure.Name = "pnlConfigure";
+            this.pnlConfigure.Size = new System.Drawing.Size(369, 141);
+            this.pnlConfigure.TabIndex = 9;
+            this.pnlConfigure.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(410, 364);
+            this.Controls.Add(this.pnlConfigure);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.txtLog);
+            this.Name = "Form1";
+            this.Text = "Concordia Device Manager";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.pnlConfigure.ResumeLayout(false);
+            this.pnlConfigure.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtSSID;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtSubscriptionId;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Panel pnlConfigure;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,116 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SerialPortLib;
 
 namespace Concordia.DeviceManager
 {
     public partial class Form1 : Form
     {
+
+        private SerialPortInput _serialPort { get; set; }
+        private bool _waitingForMessage = false;
+        private string txt;
+
+        delegate void LoggingCallback(string text);
+        delegate void StatusCallback(string text, Color color);
+        delegate void ShowConfigPanelCallback();
+
+        private void Log(string text)
+        {
+            if (this.txtLog.InvokeRequired)
+            {
+                LoggingCallback d = new LoggingCallback(Log);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.txtLog.Text = text;
+            }
+        }
+
+        private void SetStatus(string text, Color color)
+        {
+            if (this.lblStatus.InvokeRequired)
+            {
+                StatusCallback d = new StatusCallback(SetStatus);
+                this.Invoke(d, new object[] { text, color });
+            }
+            else
+            {
+                this.lblStatus.Text = text;
+                this.lblStatus.ForeColor = Color.Green;
+            }
+        }
+
+        private void ShowConfigPanel()
+        {
+            if (this.pnlConfigure.InvokeRequired)
+            {
+                ShowConfigPanelCallback d = new ShowConfigPanelCallback(ShowConfigPanel);
+                this.Invoke(d, new object[] { });
+            }
+            else
+            {
+                this.pnlConfigure.Visible = true;
+            }
+        }
+
+        public static string FromHex(string hex)
+        {
+            hex = hex.Replace("-", "");
+            byte[] raw = new byte[hex.Length / 2];
+            for (int i = 0; i < raw.Length; i++)
+            {
+                raw[i] = Convert.ToByte(hex.Substring(i * 2, 2), 16);
+            }
+            return Encoding.ASCII.GetString(raw);
+        }
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _serialPort = new SerialPortInput();
+
+            // Listen to Serial Port events
+
+            _serialPort.ConnectionStatusChanged += delegate (object sender2, ConnectionStatusChangedEventArgs args)
+            {
+                SetStatus("Waiting for connection", Color.Green);
+                Log(txt);
+            };
+
+            _serialPort.MessageReceived += delegate (object sender2, MessageReceivedEventArgs args)
+            {
+                var binaryData = BitConverter.ToString(args.Data);
+                var strData = FromHex(binaryData);
+
+                txt += string.Format("Received message: {0}", strData);
+                Log(txt);
+
+                if (strData.Contains("WAITING") && !_waitingForMessage)
+                {
+                    SetStatus("Device connection on COM3", Color.Green);
+                    ShowConfigPanel();
+                }
+            };
+
+            // Set port options
+            _serialPort.SetPort("COM3", 115200);
+
+            // Connect the serial port
+            _serialPort.Connect();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            var message = string.Format("{0}{1}{2}", txtSSID.Text.PadRight(32, ' '), txtPassword.Text.PadRight(32, ' '), txtSubscriptionId.Text.PadRight(36, ' '));
+            _waitingForMessage = true;
+            _serialPort.SendMessage(System.Text.Encoding.UTF8.GetBytes(message));
         }
     }
 }
